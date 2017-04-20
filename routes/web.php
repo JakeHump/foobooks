f75f31d968c1b1f5565c7cfd21d847117f79b7ed
Jake Humphrey <jakehumphrey@yahoo.com>
2017-04-19 23:46:38 -0400

    Commented out debug section of routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,6 @@ Route::post('/books/new', 'BookController@storeNewBook');
 Route::get('/books/{title?}', 'BookController@show');
 Route::get('/search', 'BookController@search');
 /**
-* Log viewer
-* (only accessible locally)
-*/
-if(config('app.env') == 'local') {
-    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-}
-/**
 * Practice
 */
 Route::any('/practice/{n?}', 'PracticeController@index');
@@ -22,9 +15,14 @@ Route::any('/practice/{n?}', 'PracticeController@index');
 * Main homepage visitors see when they visit just /
 */
 Route::get('/', 'WelcomeController');
-Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 if(App::environment('local')) {
+    /**
+    * Log viewer
+    * (only accessible locally)
+    */
+
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
     Route::get('/drop', function() {
 
@@ -36,39 +34,40 @@ if(App::environment('local')) {
 
 };
 
-
+/*
 Route::get('/debug', function() {
 
-	echo '<pre>';
+    	echo '<pre>';
 
-	echo '<h1>Environment</h1>';
-	echo App::environment().'</h1>';
+    	echo '<h1>Environment</h1>';
+    	echo App::environment().'</h1>';
 
-	echo '<h1>Debugging?</h1>';
-	if(config('app.debug')) echo "Yes"; else echo "No";
+    	echo '<h1>Debugging?</h1>';
+    	if(config('app.debug')) echo "Yes"; else echo "No";
 
-	echo '<h1>Database Config</h1>';
-    	echo 'DB defaultStringLength: '.Illuminate\Database\Schema\Builder::$defaultStringLength;
-    	/*
-	The following commented out line will print your MySQL credentials.
-	Uncomment this line only if you're facing difficulties connecting to the database and you
-        need to confirm your credentials.
-        When you're done debugging, comment it back out so you don't accidentally leave it
-        running on your production server, making your credentials public.
-        */
-	//print_r(config('database.connections.mysql'));
+    	echo '<h1>Database Config</h1>';
+        	echo 'DB defaultStringLength: '.Illuminate\Database\Schema\Builder::$defaultStringLength;
+      //
+      //    	The following commented out line will print your MySQL credentials.
+      //  	Uncomment this line only if you're facing difficulties connecting to the database and you
+      //        need to confirm your credentials.
+      //      When you're done debugging, comment it back out so you don't accidentally leave it
+      //    running on your production server, making your credentials public.
 
-	echo '<h1>Test Database Connection</h1>';
-	try {
-		$results = DB::select('SHOW DATABASES;');
-		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-		echo "<br><br>Your Databases:<br><br>";
-		print_r($results);
-	}
-	catch (Exception $e) {
-		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-	}
+    	//print_r(config('database.connections.mysql'));
 
-	echo '</pre>';
+    	echo '<h1>Test Database Connection</h1>';
+    	try {
+    		$results = DB::select('SHOW DATABASES;');
+    		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+    		echo "<br><br>Your Databases:<br><br>";
+    		print_r($results);
+    	}
+    	catch (Exception $e) {
+    		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    	}
+
+    	echo '</pre>';
 
 });
+*/
